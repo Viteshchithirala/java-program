936d8ec38304517eb43f195442bc1af0a9a23e18
Viteshchithirala <112738794+Viteshchithirala@users.noreply.github.com>
2022-09-22 10:28:24 +0530

    Add files via upload

---
+++ square_cube.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ class decimal 
+{
+ public static void main(String args[]) 
+{
+try
+{
+ Scanner sc = new Scanner(System.in);
+ double num;
+ System.out.print("Enter the number: ");
+ num = sc.nextDouble();
+ System.out.println("Square of " + num + " is: " + Math.pow(num, 2));
+ System.out.println("Cube of " + num + " is: " + Math.pow(num, 3));
+ }
+catch(Exception e)
+{
+System.out.println("Enter valid number");
+}
+}
+}
