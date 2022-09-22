19b5695ced4f17d378a099b5da6dd9347bda0e18
Viteshchithirala <112738794+Viteshchithirala@users.noreply.github.com>
2022-09-22 09:16:42 +0530

    Add files via upload

---
+++ perfect.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+ class perfect 
+{ 
+public static void main(String args[]) 
+{
+try{ 
+long n, sum=0; 
+Scanner sc=new Scanner(System.in); 
+System.out.print("Enter the number: "); 
+n=sc.nextLong(); 
+int i=1; 
+while(i <= n/2) 
+{ 
+if(n % i == 0) 
+{  
+sum = sum + i; 
+} 
+i++; 
+} 
+if(sum==n) 
+{  
+System.out.println(n+" is a perfect number."); 
+} 
+else  
+System.out.println(n+" is not a perfect number.");
+}
+catch(Exception e){
+	System.out.println("invalid input");
+}	 
+} 
+}
